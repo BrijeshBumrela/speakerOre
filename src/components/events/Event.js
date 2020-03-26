4f080be6f61a24bc88ebc57e4629fe7e023a98d5
tanvi217 <ivnatagarwal@gmail.com>
2020-03-26 14:39:36 +0530

    event-detail

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,9 +1,24 @@
 import React, { useContext, Fragment } from 'react';
 import EventContext from '../context/events/eventContext';
 import Navbar from '../layout/Navbar';
-import { Breadcrumb, PageHeader, Card, Button, Tag, Layout, Row } from 'antd';
+import FooterSection from '../layout/FooterSection';
+
+import {
+  Breadcrumb,
+  PageHeader,
+  Card,
+  Button,
+  Tag,
+  Layout,
+  Row,
+  Descriptions
+} from 'antd';
 import { HomeOutlined, HeartTwoTone } from '@ant-design/icons';
 import Paragraph from 'antd/lib/skeleton/Paragraph';
+
+import './style.css';
+import DescriptionsItem from 'antd/lib/descriptions/Item';
+import Meta from 'antd/lib/card/Meta';
 
 const { Content } = Layout;
 
@@ -22,7 +37,9 @@ const Event = ({ match }) => {
     about,
     street,
     city,
+    state,
     country,
+    postalcode,
     start_date,
     end_date,
     categories,
@@ -33,9 +50,12 @@ const Event = ({ match }) => {
     website
   } = events[0];
 
+  const loc =
+    street + ', ' + city + ', ' + state + ', ' + country + '\n' + postalcode;
+
   return (
     <Fragment>
-      <Navbar heading={'Event Detail ' + event_id} />
+      <Navbar heading={'Event - ' + name} />
       <Breadcrumb style={{ margin: '16px 0', padding: '10px 100px' }}>
         <Breadcrumb.Item>
           <a href='/'>
@@ -47,30 +67,56 @@ const Event = ({ match }) => {
         </Breadcrumb.Item>
         <Breadcrumb.Item>{name}</Breadcrumb.Item>
       </Breadcrumb>
-      <Content>
-        <PageHeader
-          title={name}
-          className='site-page-header'
-          extra={[
-            <Button key='1'>
-              {
-                <span>
-                  <HeartTwoTone /> Bookmark
-                </span>
-              }
-            </Button>
-          ]}
+      <Content
+        className='site-layout'
+        style={{
+          padding: '0 100px',
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}
+      >
+        <Card
+          className='site-layout-background'
+          style={{
+            padding: '15px',
+            flex: '2 1 auto',
+            margin: '0 5px 0 0'
+          }}
         >
-          {' '}
-          <Content>
-            <Paragraph>
-              <Row>{about}</Row>
-            </Paragraph>
-
-            <Paragraph>{description}</Paragraph>
-          </Content>
-        </PageHeader>
+          <h1>{name}</h1>
+          <Meta title='Start Date' description={start_date}></Meta>
+          <br />
+          <Meta title='End Date' description={end_date}></Meta>
+          <br />
+          <Meta title='About' description={description}></Meta>
+          <br />
+          <Meta title='Location' description={loc}></Meta>
+          <br />
+          <div>
+            {tags.map((tag, index) => (
+              <Tag color='#0f74a8' key={index}>
+                {tag.toUpperCase()}
+              </Tag>
+            ))}
+          </div>
+        </Card>
+        <Card
+          className='site-layout-background'
+          title={'Contact'}
+          style={{
+            padding: '5px',
+            flex: '1 1 auto',
+            margin: '0px 0 0 10px'
+          }}
+        >
+          <Meta title='Website' description={website} />
+          <br />
+          <Meta title='Email' description={email} />
+          <br />
+          <Meta title='Phone' description={phone} />
+        </Card>
       </Content>
+      <FooterSection />
     </Fragment>
   );
 };
